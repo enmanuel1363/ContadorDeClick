3ee89a34f30ad580105141bbd763594ce2e32bbe
enmauel1363 <enmanuelzamora1363@gmail.com>
2025-04-23 21:38:04 -0600

    se agrega el proyecto y primer version de el contador con los botones malos

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Boton from './Componentes/Boton';
+import { useState } from 'react';
+
 
 export default function App() {
+
+const [contador, setContador] = useState(0);
+
+const FuncionMas = () => {
+  setContador(contador + 1)
+}
+
+const FuncionReload = () => {
+  setContador(0)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={styles.containerLogo}>
+      <Image source={require('./Imagenes/logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.containerBoton}>
+        <View style={styles.containerContador}>
+          <Text style={styles.numero}>{contador}</Text>
+          </View> 
+        <Boton 
+          boton="clic"
+          color='blue'
+          colorText='white'
+          onPress={FuncionMas}>
+        </Boton>
+
+        <Boton
+          boton="Reiniciar"
+          color='green'
+          colorText='white'
+          onPress={FuncionReload}>
+        </Boton>
+
+      </View>
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0B0B17',
+ 
   },
+  containerBoton: {
+    flex: 3,
+    width: '100%',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+
+  containerContador: {
+ backgroundColor: '0B0B17',
+ width: '60%',
+ height: 200,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: 'white',
+  borderWidth: 5,
+  marginTop: 10, 
+  },
+  containerLogo: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: '#0B0B17',
+    
+  },
+  numero: {
+    fontSize: 100,
+    color: 'white',
+    paddingLeft: 20
+  },
+  logo: {
+    height:200,
+    width: "100%",
+  },
+  
 });
---
+++ Componentes/Boton.js
@@ -0,0 +1,52 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+
+function Boton(props) {
+    return (
+
+        <View style={styles.contianer}>
+            <View style={styles.botonContainer}>
+                <TouchableOpacity style={[
+        styles.boton, { backgroundColor: props.color }]}
+         onPress={props.onPress}>
+
+                    <Text style={[styles.botonT, { color: props.colorText }]}>{props.boton}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+export default Boton;
+
+const styles = StyleSheet.create({
+    contianer:{
+        alingItems:'center',
+        justifyContent:'center',
+    },
+
+    botonContainer:{
+       alingItems:'center',
+     
+    },
+    boton:{
+        
+        borderRadius: 10,
+        height: 70,
+        whitd: 205,
+        borderColor: 'white',
+        borderWhidth: 2,
+        justifyContent:'center',
+        shadowncolor: 'white',
+        shadowOffset: { width: 0, height: 9 },
+        shadowOpacity: 0.44,
+        shadowRadius: 10.32,
+        elevation: 6,
+    },
+    botonT:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        
+       
+    },
+})
